849344d78669d973fcc3a0cbc8b3e4df50afd0eb
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-08-19 10:47:04 +0300

    Initial commit

---
+++ src/main/java/com/example/springinprod/SpringInProdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInProdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInProdApplication.class, args);
+    }
+
+}
